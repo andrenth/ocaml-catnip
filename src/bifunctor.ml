f0415ebc473607975125eb047d6b028e1e8a5c52
Andre Nathan <andre@digirati.com.br>
2017-04-10 14:39:19 -0300

    Use Prelude

--- src/bifunctor.ml
+++ src/bifunctor.ml
@@ -1,3 +1,5 @@
+open Prelude
+
 module type I = sig
   type ('a, 'b) t
   val bimap : ('a -> 'c) -> ('b -> 'd) -> ('a, 'b) t -> ('c, 'd) t
@@ -10,8 +12,6 @@ end
 
 module Make (B : I) : S with type ('a, 'b) t := ('a, 'b) B.t = struct
   include B
-
-  let id x = x
 
   let first  f = B.bimap f id
   let second f = B.bimap id f
